506d3a838258610b308f8dff457f7b8839ba633f
mashixaal <safwan7342@gmail.com>
2022-09-29 16:53:52 +0200

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+// import"../assets/css/styles.css"
+import "tailwindcss/tailwind.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
-}
+};
 
 export default MyApp
